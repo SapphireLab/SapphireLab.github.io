6c517eeb2dd2363730904c58a6e8b063ed7924f7
starylan <starylan@outlook.com>
2025-12-05 00:24:28 +0800

    添加链接插件

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,6 +23,7 @@ import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-copy-button.js";
+import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
 
 // https://astro.build/config
 export default defineConfig({
@@ -113,6 +114,7 @@ export default defineConfig({
 			parseDirectiveNode,
 		],
 		rehypePlugins: [
+			[rehypeAstroRelativeMarkdownLinks, {trailingSlash: "always"}],
 			rehypeKatex,
 			rehypeSlug,
 			[
